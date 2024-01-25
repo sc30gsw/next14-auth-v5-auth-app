5bf907964a9fae7486453bfc634ad17b8869bcd1
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-01-26 08:03:45 +0900

    ✨ server actionsにして、prisma連携

--- src/actions/signUp.ts
+++ src/actions/signUp.ts
@@ -1,6 +1,12 @@
-import { SignUpFormInput, signUpSchema } from '@/types/schemas/signUpSchema'
+'use server'
 
-export const signUp = async (data: SignUpFormInput) => {
+import { prisma } from '@/lib/db'
+import { handleError } from '@/lib/utils'
+import { ActionsResult } from '@/types/ActionResult'
+import { SignUpFormInput, signUpSchema } from '@/types/schemas/signUpSchema'
+import bcrypt from 'bcrypt'
+
+export const signUp = async (data: SignUpFormInput): Promise<ActionsResult> => {
   const result = signUpSchema.safeParse(data)
 
   if (!result.success) {
@@ -12,8 +18,41 @@ export const signUp = async (data: SignUpFormInput) => {
     }
   }
 
-  return {
-    isSuccess: true,
-    message: 'サインアップに成功しました',
+  const { nickname, email, password } = result.data
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 12)
+    console.log('🚀 ~ signUp ~ hashedPassword:', hashedPassword)
+
+    const existingUser = await prisma.user.findUnique({ where: { email } })
+    console.log('🚀 ~ signUp ~ existingUser:', existingUser)
+
+    if (existingUser) {
+      return {
+        isSuccess: false,
+        error: {
+          message: 'このメールアドレスは既に登録されています。',
+        },
+      }
+    }
+
+    const user = await prisma.user.create({
+      data: { name: nickname, email, password: hashedPassword },
+    })
+    console.log('🚀 ~ signUp ~ user:', user)
+
+    return {
+      isSuccess: true,
+      message: 'サインアップに成功しました',
+    }
+  } catch (err) {
+    handleError(err)
+
+    return {
+      isSuccess: false,
+      error: {
+        message: 'サインアップに失敗しました。',
+      },
+    }
   }
 }
