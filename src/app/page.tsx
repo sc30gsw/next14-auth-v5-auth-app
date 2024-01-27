0e53fc6e7cbe5fc8276637acd2c1f4a6f6d83c63
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-01-28 07:46:09 +0900

    ✨ signOut機能の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,31 @@
-const Home = () => {
-  return <div>Home</div>
+import React from 'react'
+
+import { Button } from '@/components/ui/button'
+import { auth, signOut } from '../../auth'
+
+const Home = async () => {
+  const session = await auth()
+  console.table(session)
+
+  if (!session) {
+    return null
+  }
+
+  return (
+    <div>
+      <form
+        action={async () => {
+          'use server'
+
+          await signOut({ redirectTo: '/sign-in' })
+        }}
+      >
+        <Button variant={'ghost'} type="submit">
+          Sign Out
+        </Button>
+      </form>
+    </div>
+  )
 }
 
 export default Home
