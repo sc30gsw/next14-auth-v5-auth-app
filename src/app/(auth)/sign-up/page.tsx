0fd3dda02885da21c15e47cd56e0e37d10213205
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-01-27 17:25:06 +0900

    💄 スタイリングの修正

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,15 +1,17 @@
 import { SignUpForm } from '@/app/components/auth/SignUpForm'
+import { SocialButtons } from '@/app/components/auth/SocialButtons'
 import React from 'react'
 import { tv } from 'tailwind-variants'
 
 const signUpPageStyles = tv({
-  base: 'flex justify-center items-center my-5',
+  base: 'flex flex-col justify-center items-center my-5',
 })
 
 const SignUpPage = () => {
   return (
     <div className={signUpPageStyles()}>
       <SignUpForm />
+      <SocialButtons />
     </div>
   )
 }
